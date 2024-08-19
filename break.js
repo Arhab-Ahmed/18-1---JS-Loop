09c0321ccaa9ef72bf2c4eba03400c43df703747
unknown <you@example.com>
2024-08-19 23:23:12 +0600

    hahah

---
+++ break.js
@@ -0,0 +1,16 @@
+// for(let i = 1; i <= 10; i++){
+//     if( i > 5){
+//         break;
+//     }
+//     console.log(i)
+// }
+let i = 0;
+
+while(i < 20){
+    if( i > 10){
+        break;
+    }
+    console.log(i);
+    i++;
+    
+}
---
+++ continue.js
@@ -0,0 +1,18 @@
+// for(let i = 1; i < 20; i++){
+//     // i ke 5 dara vivaggo kora gele skip korba 
+//     if( i % 5 === 0 ){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+let i = 0
+while(i < 50){
+    i++
+    // i ke 5 dara vivajjo kora na gele skip kore diba
+    if( i % 5 !== 0){
+        continue;
+    }
+    console.log(i);
+}
+ 
